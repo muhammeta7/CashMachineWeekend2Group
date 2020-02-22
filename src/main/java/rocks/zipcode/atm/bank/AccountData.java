4bb53b12ceabe206cd8a997875345b10c1cf3a43
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-02-21 21:42:26 -0500

    added pin field and constructor.

--- src/main/java/rocks/zipcode/atm/bank/AccountData.java
+++ src/main/java/rocks/zipcode/atm/bank/AccountData.java
@@ -8,14 +8,16 @@ public final class AccountData {
     private final int id;
     private final String name;
     private final String email;
-
+    // TODO: Need to change balance into Double
     private final int balance;
+    private final String pin;
 
-    AccountData(int id, String name, String email, int balance) {
+    AccountData(int id, String name, String email, int balance, String pin) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.balance = balance;
+        this.pin = pin;
     }
 
     public int getId() {
@@ -30,15 +32,22 @@ public final class AccountData {
         return email;
     }
 
+    // TODO need to change into Double type
     public int getBalance() {
         return balance;
     }
 
+    public String getPin() {
+        return pin;
+    }
+
     @Override
     public String toString() {
+        String formatBalance = String.format("%1$,.2f", balance);
+
         return "Account id: " + id + '\n' +
                 "Name: " + name + '\n' +
                 "Email: " + email + '\n' +
-                "Balance: " + balance;
+                "Balance: $" + formatBalance;
     }
 }
