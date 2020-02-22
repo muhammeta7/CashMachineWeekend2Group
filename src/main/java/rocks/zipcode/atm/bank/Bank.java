9f45fa8a23ce4ffeae66b323f8e623b1a3402510
Muhammet Aydin <muhammeta7@gmail.com>
2020-02-22 10:04:25 -0500

    Revert "Added method to create new account"

--- src/main/java/rocks/zipcode/atm/bank/Bank.java
+++ src/main/java/rocks/zipcode/atm/bank/Bank.java
@@ -22,38 +22,15 @@ public class Bank {
         )));
     }
 
-    // Create New Account
-    public ActionResult<AccountData> addNewAccount(int id, String name, String email, int balance, String accountType) {
-
-        if (accountType.equals("basic")){
-            accounts.put(id, new BasicAccount(new AccountData(id, name, email, balance)));
-        }
-
-        if(accountType.equals("premium")){
-            accounts.put(id, new PremiumAccount(new AccountData(id,name,email,balance)));
-        }
-
-        Account newAccount = accounts.get(id);
-
-        return ActionResult.success(newAccount.getAccountData());
-
-    }
-
-    // Get Account by ID
     public ActionResult<AccountData> getAccountById(int id) {
         Account account = accounts.get(id);
 
         if (account != null) {
             return ActionResult.success(account.getAccountData());
         } else {
-            // TODO: update failure message
             return ActionResult.fail("No account with id: " + id + "\nTry account 1000 or 2000");
         }
     }
-
-
-    // TODO: add checkPin method
-    // TODO: if we wan to reset a pin
 
     public ActionResult<AccountData> deposit(AccountData accountData, int amount) {
         Account account = accounts.get(accountData.getId());
@@ -69,12 +46,7 @@ public class Bank {
         if (ok) {
             return ActionResult.success(account.getAccountData());
         } else {
-            // TODO format string update failure message
-            // TODO format amountString
-            // TODO format balanceString
             return ActionResult.fail("Withdraw failed: " + amount + ". Account has: " + account.getBalance());
         }
     }
-
-
 }
